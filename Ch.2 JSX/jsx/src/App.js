b086b569cc4ace1fe32192d32320712bd55e345d
DESKTOP-3IVCKQQ\lkas7 <lkas78@naver.com>
2023-05-01 10:39:55 +0900

    Ch.2 JSX
       # JSX 기본 문법 ( 조건연산자, &&, || 등 )
       # Inline Styling
       # className
       # ESLint, Prettier

---
+++ Ch.2 JSX/jsx/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  const name = '리액트';
+  const style = {
+    color: 'red',
+    backgroundColor: 'black'
+  };
+
+  return (
+    <div style={style}>
+      {name}
+    </div>
+  )
+}
+
+export default App;
