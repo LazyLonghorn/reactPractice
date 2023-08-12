59dafdeca52f5f2668fb59b42cfdbcd06f86da04
DESKTOP-3IVCKQQ\lkas7 <lkas78@naver.com>
2023-08-12 15:18:06 +0900

    Ch.12 immer를 사용하여 더 쉽게 불변성 유지하기
       # 컴포넌트의 상태를 업데이트할 때, 중요한 점은 불변성을 지키는 것이다.
          이런 불변성은 유지할 때, 객체의 구조가 복잡하면 불편할 수 있다.
    
          이런 과정에서 immer 을 사용하면 쉽게 가능.
    
       # immer 의 produce 함수를 사용한다.
         - 첫 번째 파라미터 : 수정하고 싶은 상태
         - 두 번째 파라마티 : 상태를 어떻게 업데이트할지 정의하는 함수
    
       # 첫 번째 파라미터를 생략하면, 업데이트 함수를 반환하고, 이를 useState 의 함수형 업데이트와 함께 사용할 수 있다.

--- Ch.12 immer를 사용하여 더 쉽게 불변성 유지하기/immer-tutorial/src/App.js
+++ Ch.12 immer를 사용하여 더 쉽게 불변성 유지하기/immer-tutorial/src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef, useState } from 'react';
+import { produce } from 'immer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const nextId = useRef(1);
+    const [form, setForm] = useState({ name: '', username: '' });
+    const [data, setData] = useState({
+        array: [],
+        uselessValue: null
+    });
+
+    const onChange = useCallback((e) => {
+        const {name, value} = e.target;
+        // setForm({
+        //     ...form,
+        //     [name]: value
+        // });
+
+        setForm(
+            // produce(form, draft => {
+            //     draft[name] = value;
+            // })
+            produce(draft => {
+                draft[name] = value;
+            })
+        );
+    }, [form]);
+
+    const onClick = useCallback(e => {
+        e.preventDefault();
+
+        const info = {
+            id: nextId.current,
+            name: form.name,
+            username: form.username
+        };
+
+        // setData({
+        //     ...data,
+        //     array: data.array.concat(info)
+        // });
+
+        setData(
+            // produce(data, draft => {
+            //     draft.array.push(info);
+            // })
+            produce(draft => {
+                draft.array.push(info);
+            })
+        );
+
+        setForm({ name: '', username: '' });
+
+        nextId.current += 1;
+    }, [data, form.name, form.username]);
+
+    const onRemove = useCallback((id) => {
+        // setData({
+        //     ...data,
+        //     array: data.array.filter((info) => id !== info.id)
+        // })
+        setData(
+            // produce(data, draft => {
+            //     draft.array = draft.array.filter((info) => {
+            //         console.log(id !== info.id)
+            //         return id !== info.id
+            //     })
+            // })
+            produce(draft => {
+                draft.array = draft.array.filter((info) => {
+                    console.log(id !== info.id)
+                    return id !== info.id
+                })
+            })
+        )
+    }, [data]);
+    
+
+    return (
+        <div>
+            <form>
+                <input 
+                    name='username'
+                    placeholder='ID'
+                    value={form.username}
+                    onChange={onChange}
+                />
+                <input 
+                    name='name'
+                    placeholder='Name'
+                    value={form.name}
+                    onChange={onChange}
+                />
+                <button onClick={onClick}>Add</button>
+            </form>
+            <div>
+                <ul>
+                    {data.array.map((info) => 
+                        <li key={info.id} onClick={() => onRemove(info.id)}>
+                            {info.username} ({info.name})
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
+};
 
 export default App;
